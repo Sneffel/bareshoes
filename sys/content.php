cc1163c894d973b5bb6d113c140ddc30e51cffd0
Bruno <zeroeiter@gmail.com>
2024-01-05 13:49:08 +0100

    basic tags, could be improved, I don't like the ./ but can be replaced with ../ for the moment based on location, sad

---
+++ sys/content.php
@@ -0,0 +1,15 @@
+<?php
+
+$headText = '<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link href="./theme.css" rel="stylesheet">
+<!-- MMMMMMMMMMMH! -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@900&family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,900;1,400&display=swap" rel="stylesheet">';
+
+
+$scriptsText = '<script src="./sys/theme.js"></script>';
